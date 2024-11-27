0ab426a5d4e941a2a52dcaac4abf28ea39d6fa8d
yujihun <sk0253642@gmail.com>
2024-11-27 11:12:32 +0900

    articles

---
+++ src/main/java/com/rest/domain/article/controller/ApiV1ArticleController.java
@@ -0,0 +1,25 @@
+package com.rest.domain.article.controller;
+
+import com.rest.domain.article.entity.Article;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/articles")
+public class ApiV1ArticleController {
+    @GetMapping("")
+    public List<Article> getArticles(){
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article(1L));
+        articles.add(new Article(2L));
+        articles.add(new Article(3L));
+
+        return articles;
+    }
+}
---
+++ src/main/java/com/rest/domain/article/entity/Article.java
@@ -0,0 +1,11 @@
+package com.rest.domain.article.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Article {
+
+    private  Long id;
+}
